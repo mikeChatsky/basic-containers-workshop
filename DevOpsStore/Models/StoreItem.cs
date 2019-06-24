4f436a4365dc2e5f28ca2ef791022f75f8f8ac99
Mike Chatsky <mike20016@gmail.com>
2019-06-25 00:26:52 +0300

    init .net core project

---
+++ DevOpsStore/Models/StoreItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevOpsStore.Models
+{
+    public class StoreItem : DbContext
+    {
+        public string Id { get; set; }
+    }
+}
