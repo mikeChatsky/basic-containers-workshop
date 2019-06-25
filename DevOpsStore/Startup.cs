3daf192fc3541a5e5ad5de1ed48fd1f70661ea29
Mike Chatsky <mike20016@gmail.com>
2019-06-25 23:32:58 +0300

    refactor(client-side): move web app to new location & serving mechanism

--- DevOpsStore/Startup.cs
+++ DevOpsStore/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
@@ -17,14 +18,14 @@ namespace DevOpsStore
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "WebApp/build"; });
+
             services.AddDbContext<DevOpsStoreContext>(options =>
                 options.UseMySQL(Configuration.GetConnectionString("mysql")));
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,13 +37,29 @@ namespace DevOpsStore
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseSpaStaticFiles();
             app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "WebApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
         }
     }
 }
--- DevOpsStore/WebApp/src/App.js
+++ DevOpsStore/WebApp/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { async } from 'q';
 
 function App() {
+  const [items, setItems] = useState({ items: [] });
+
+  useEffect(async () => {
+    
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          
+
         </div>
       </header>
     </div>
